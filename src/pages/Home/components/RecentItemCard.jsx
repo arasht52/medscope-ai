d6523f4022b98a42c6a6d14e95d181f2a3396628
arasht52 <arasht52@gmail.com>
2026-06-20 03:18:29 +0200

    Create RecentItemCard.jsx

---
+++ src/pages/Home/components/RecentItemCard.jsx
@@ -0,0 +1,16 @@
+export default function RecentItemCard() {
+  return (
+    <div className="recent-card">
+      <h3>آخرین مطالعه</h3>
+
+      <div className="recent-item">
+        <div>
+          <strong>Simple Columnar Epithelium</strong>
+          <p>Histology</p>
+        </div>
+
+        <button>ادامه</button>
+      </div>
+    </div>
+  );
+}
